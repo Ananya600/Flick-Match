5f069d4ca27de0afd7d14b99d53cf0b0059305c0
Ananya600 <ananyarameshk@gmail.com>
2025-02-18 06:28:14 +0530

    Update constants.dart

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,5 +1,5 @@
 class ApiKey{
-  static const String apiKey = '11fc0d9e1bda516cfa24f94791f9f6fd';
+  static const String apiKey = Your_Api_Key;
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 }
